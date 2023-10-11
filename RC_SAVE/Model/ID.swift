fd8d104620254f600e6cc3ef3a0abe22033f9a5e
sako-0715 <sakou.0715@gmail.com>
2023-10-12 00:47:19 +0900

    RealmとDBにIDを保存し整合性がとれていることを確認済

---
+++ RC_SAVE/Model/ID.swift
@@ -0,0 +1,8 @@
+//
+//  ID.swift
+//  RC_SAVE
+//
+//  Created by 酒匂竜也 on 2023/10/07.
+//
+
+import Foundation
---
+++ RC_SAVE/Model/ImaegRealm.swift
@@ -0,0 +1,8 @@
+//
+//  ImaegRealm.swift
+//  RC_SAVE
+//
+//  Created by 酒匂竜也 on 2023/10/08.
+//
+
+import Foundation
