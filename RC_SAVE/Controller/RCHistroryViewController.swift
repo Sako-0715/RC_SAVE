4abb848fae13f2188631ea04b9f51fadab0ec8a0
sako-0715 <sakou.0715@gmail.com>
2023-10-22 19:57:02 +0900

    Realmに投稿する修正追加、日時をRealmに投稿を追加、Realmより画像を取得、日時が新しいのを履歴の上に持ってくる

---
+++ RC_SAVE/Controller/RCHistroryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RCHistroryViewController.swift
+//  RC_SAVE
+//
+//  Created by 酒匂竜也 on 2023/10/12.
+//
+
+import UIKit
+
+class RCHistroryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RC_SAVE/Model/HistoryModel.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryModel.swift
+//  RC_SAVE
+//
+//  Created by 酒匂竜也 on 2023/10/21.
+//
+
+import Foundation
