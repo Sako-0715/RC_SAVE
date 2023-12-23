b0a0b1ceb2298f976e047e11a14d6ee7ec378349
sako-0715 <sakou.0715@gmail.com>
2023-12-23 19:15:30 +0900

    投稿のAPI通信処理追加、投稿履歴のAPI通信の処理追加

---
+++ RC_SAVE/Controller/RCSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RCSearchViewController.swift
+//  RC_SAVE
+//
+//  Created by 酒匂竜也 on 2023/10/28.
+//
+
+import UIKit
+
+class RCSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
